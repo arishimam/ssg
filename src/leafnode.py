3e7cd06e13e36fdb53bd8b53434bf99eba9a1741
Arish Imam <arish.imam@gmail.com>
2025-02-07 10:40:05 -0800

    parentnode added plus tests

--- src/leafnode.py
+++ src/leafnode.py
@@ -2,15 +2,16 @@ from htmlnode import HTMLNode
 
 class LeafNode(HTMLNode):
     def __init__(self, tag=None, value=None, props=None):
-        super().__init__(tag, value, props=props)
+        super().__init__(tag, value, None, props)
 
     def __to_html__(self):
         if self.value == None:
             raise ValueError()
         if self.tag == None:
             return self.value
-        if self.props == None:
-            return f"<{self.tag}>{self.value}</{self.tag}>"
+#        if self.props == None:
+#            return f"<{self.tag}>{self.value}</{self.tag}>"
+
         return f"<{self.tag}{self.__props_to_html__()}>{self.value}</{self.tag}>"
 
     def __repr__(self):
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -18,12 +18,30 @@ class TestHTMLNode(unittest.TestCase):
         node2 = HTMLNode("p","random info")
         self.assertNotEqual(node1, node2)
 
+    def test_values(self):
+        node = HTMLNode(
+            "div",
+            "im just taking space",
+            None,
+            None
+        )
+        self.assertEqual("div", node.tag)
+        self.assertEqual("im just taking space", node.value)
+        self.assertEqual(None, node.children)
+        self.assertEqual(None, node.props)
+
     def test_props(self):
-        node1 = HTMLNode(props={"href":"https://www.google.com", "target":"_blank",})
+        node1 = HTMLNode(
+            "div",
+            "Hello there!",
+            None,
+            {"href":"https://www.google.com", "target":"_blank",}
+        )
         self.assertEqual(' href="https://www.google.com" target="_blank"', node1.__props_to_html__())
-        
 
     def test_repr(self):
         node1 = HTMLNode("p", "Random info in tag", None, {"href":"www.google.com"})
         self.assertEqual("HTMLNode(tag=p, value=Random info in tag, children=None, props={'href': 'www.google.com'})", repr(node1))
 
+if __name__ == "__main__":
+    unittest.main()
